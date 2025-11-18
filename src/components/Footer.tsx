561cf684bf12ac12c3f6b81372c8fdbc0b72850d
Ravi Patel <ravikumar.patel.if@gmail.com>
2025-11-18 11:49:34 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className="container mx-auto px-6">
         <div className="text-center">
           <p className="text-muted-foreground">
-            © {new Date().getFullYear()} Ravi Patel.All rights reserved.
+            © {new Date().getFullYear()} Ravi Patel. All rights reserved.
           </p>
         </div>
       </div>
