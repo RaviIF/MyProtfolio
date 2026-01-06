43a6f9cd17c7d212e80b0ae91352d29036312992
Ravi Patel <ravikumar.patel.if@gmail.com>
2026-01-06 15:54:00 +0530

    Integrate Toaster and change Router basename
    Added Toaster component for notifications and updated basename.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { Toaster } from "@/components/ui/toaster";
 const App = () => (
-  <BrowserRouter basename="/MyProtfolio">
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+  <BrowserRouter basename="/Protfolio">
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {/* Toast renderer – MUST be here */}
+      <Toaster />
+    </>
   </BrowserRouter>
 );
 export default App;
